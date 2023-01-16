12c463cea1deb4c9854762020c3749544341c611
StarGrim33 <prosky33@gmail.com>
2023-01-16 14:51:20 +0300

    Выполнение задачи

---
+++ HomeWorkOOP9/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeWorkOOP9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Mall
+    {
+
+        private Queue<Client> _clients = new Queue<Client>();
+    }
+
+    class Client
+    {
+        private int _money;
+        
+    }
+
+
+}
